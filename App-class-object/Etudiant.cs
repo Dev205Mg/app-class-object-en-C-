040479cc6ee123ed03cfac83b7ad21f26bd10474
Dev205 <rodyael.205@gmail.com>
2023-03-10 09:05:30 +0300

    Initiation au POO du C#

---
+++ App-class-object/Etudiant.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App_class_object
+{
+    class Etudiant
+    {
+        public string nom, mention;
+        public float note1, note2, moyenne;
+
+        public Etudiant()
+        {
+        }
+
+        public void setNote()
+        {
+            Console.Write("Nom = ");
+            nom = Console.ReadLine();
+            Console.Write("Note 1 = ");
+            note1 = float.Parse(Console.ReadLine());
+            Console.Write("Note 2 = ");
+            note2 = float.Parse(Console.ReadLine());
+        }
+        public void calcMoyenGe()
+        {
+            moyenne = (this.note1 + this.note2) / 2;
+        }
+        public void attribMention()
+        {
+            if(moyenne <= 10)
+            {
+                mention = " Miezaha fa tsy ampy !!";
+            } else if (moyenne <= 15)
+            {
+                mention = " Tsara fa tohizo ";
+            }
+            else
+            {
+                mention = " Tena tsara ";
+            }
+            
+        }
+        public void imprimBulletin()
+        {
+            Console.WriteLine("Nom = " + nom);
+            Console.WriteLine("Moyenne = " + moyenne);
+            Console.WriteLine("Mention = " + mention);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ App-class-object/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App_class_object
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Etudiant rakoto = new Etudiant();
+            rakoto.setNote();
+            rakoto.calcMoyenGe();
+            rakoto.attribMention();
+            rakoto.imprimBulletin();
+        }
+    }
+}
